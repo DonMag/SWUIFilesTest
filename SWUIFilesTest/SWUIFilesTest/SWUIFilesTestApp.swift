5742bd33b852c46978433a1c5a506b33070008e2
DonMag <don_mail_2000@yahoo.com>
2021-07-23 16:01:46 -0400

    First Look

---
+++ SWUIFilesTest/SWUIFilesTest/SWUIFilesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWUIFilesTestApp.swift
+//  SWUIFilesTest
+//
+//  Created by Don Mag on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SWUIFilesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
